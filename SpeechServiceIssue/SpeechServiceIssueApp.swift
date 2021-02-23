206e9231a971edaf1f4a80301ea16bcc53ba7c08
Alexandr Afanasiev <oleksandr.afanasiev@masterofcode.com>
2021-02-23 11:15:33 +0200

    Initial Commit

---
+++ SpeechServiceIssue/SpeechServiceIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechServiceIssueApp.swift
+//  SpeechServiceIssue
+//
+//  Created by Aleksandr on 23.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SpeechServiceIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
